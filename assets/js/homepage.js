3543adc6f9e150c504644e42d58336754fb72a04
Zach Oda <zjodachow@gmail.com>
2022-07-05 12:06:39 -0400

    optimized getUserRepos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,11 @@
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    // format the github API URL
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the URL
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
-getUserRepos();
